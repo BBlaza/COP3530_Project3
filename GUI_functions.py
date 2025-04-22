88c680af2d43df38c371ddbed34393c159e22c8b
Brazza <jjia@ufl.edu>
2025-04-21 21:52:52 -0400

    Update

--- GUI_functions.py
+++ GUI_functions.py
@@ -1,6 +1,7 @@
 from db import get_cursor
 import elements
 import time
+import SQLite
 
 def Btree_search():
     title = elements.search_entry.get().strip()
@@ -107,12 +108,55 @@ def Btree_search():
     for row in rows:
         elements.results_tree.insert("", "end", values=row)
 
+
+
 def Bptree_search():
-    db = MovieDatabase('movies.db', 'title.basics.tsv.gz')
+    db = SQLite.MovieDatabaseSQLite('movies.db', 'title.basics.tsv.gz')
     db.load_ratings('title.ratings.tsv.gz')
 
+    title = elements.search_entry.get().strip()
 
-    multi_results = db.get_movies_by_filters(genre="Action", ratingMin=7.5)
-   
+    min_runtime, max_runtime = 0, 0
+    if (elements.duration_range_combo.get() == 'less than 30 hours'):
+        min_runtime = -1
+        max_runtime = 29
+    elif (elements.duration_range_combo.get() == '31-59 min'):
+        min_runtime = 30
+        max_runtime = 59
+    elif (elements.duration_range_combo.get() == '1-2 hours'):
+        min_runtime = 60
+        max_runtime = 119
+    elif (elements.duration_range_combo.get() == '2-3 hours'):
+        min_runtime = 120
+        max_runtime = 179
+    elif (elements.duration_range_combo.get() == '3-5 hours'):
+        min_runtime = 180
+        max_runtime = 299
+    elif (elements.duration_range_combo.get() == '5-10 hours'):
+        min_runtime = 300
+        max_runtime = 5999
+    elif (elements.duration_range_combo.get() == 'more than 10 hours'):
+        min_runtime = 6000
+        max_runtime = 9999999
+
+    min_release_date = elements.release_scale_min.get()
+    max_release_date = elements.release_scale_max.get()
+
+    genre = elements.genre_combo.get()
+
+    lowest_rate = elements.rating_scale_min.get()
+    highest_rate = elements.rating_scale_max.get()
+
+    if (min_release_date > max_release_date) or (lowest_rate > highest_rate):
+        elements.log_label.config(text="Maximum cannot be greater than minimum", fg="red")
+        return
+
+    rows = db.get_movies_by_filters(genre=genre, movieName=title, ratingMin=min_runtime, ratingMax=max_runtime, startingYear=min_release_date, endingYear=max_release_date)
+
+    for item in elements.results_tree.get_children():
+        elements.results_tree.delete(item)
+
+    for row in rows:
+        elements.results_tree.insert("", "end", values=row)
 
     db.close()
